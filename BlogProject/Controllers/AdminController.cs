1480fe4fbf57ff96eab417d095adcb8d14ded1ae
Murat Kaplan <muratkaplan061989@gmail.com>
2023-12-23 01:33:48 +0300

    Dropdown olusturuldu, validation kuruldu

--- BlogProject/Controllers/AdminController.cs
+++ BlogProject/Controllers/AdminController.cs
@@ -1,9 +1,8 @@
-﻿using BusinessLayer.Concrete;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using BusinessLayer.Concrete;
 using System.Web.Mvc;
+using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
 
 namespace BlogProject.Controllers
@@ -11,6 +10,7 @@ namespace BlogProject.Controllers
     public class AdminController : Controller
     {
         private readonly BlogManager _blogManager = new BlogManager();
+        private readonly CategoryManager _categoryManager = new CategoryManager();
 
         public ActionResult Index()
         {
@@ -25,14 +25,39 @@ namespace BlogProject.Controllers
         [HttpGet]
         public ActionResult AddBlog()
         {
+            Context context = new Context();
+            var categoryList = (from x in context.Categories.ToList()
+                select new SelectListItem
+                {
+                    Text = x.CategoryName,
+                    Value = x.CategoryId.ToString()
+                }).ToList();
+
+            ViewBag.categoryList = categoryList;
             return View();
         }
 
         [HttpPost]
         public ActionResult AddBlog(Blog blog)
         {
-            _blogManager.AddBlog(blog);
-            return RedirectToAction("AdminBlogList");
+            if (ModelState.IsValid)
+            {
+                _blogManager.AddBlog(blog);
+                return RedirectToAction("AdminBlogList");
+            }
+            Context context = new Context();
+            var categoryList = (from x in context.Categories.ToList()
+                select new SelectListItem
+                {
+                    Text = x.CategoryName,
+                    Value = x.CategoryId.ToString()
+                }).ToList();
+
+            ViewBag.categoryList = categoryList;
+
+            
+            return View(blog);
+
         }
     }
 }
